3f1476bfc8f0da6005a9f91cd182cb27b68e4bd7
Kartik Pandey <karthikpandey580@gmail.com>
2025-01-23 18:21:48 +0530

    Create loopAndprintingPattern.cpp

---
+++ loopAndprintingPattern.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a;
+    // cin>>a;
+    // int b = 1;
+    // while(b<=a){
+    //     cout<<b;
+    //     b++;
+    // }
+
+    // int a ;
+    // cin>>a;
+    // int sum = 0;
+    // int i = 1;
+    // while(i<=a){
+    //     sum = i+sum;
+    //     i++;
+    // }
+    // cout<<"Sum of given number is : " << sum <<endl;
+
+   /* int n;
+    cin >> n;
+    int sum = 0;
+    int b = 2;
+    while(b<=n){
+        sum = sum + b;
+        b++;
+    }
+    cout << sum;*/
+
+    // int a;
+    // cin>>a;
+    // int b = 1;
+    // while(b<=a){
+    //     cout<<b;
+    //     b++;
+    // }
+
+    // int a ;
+    // cin>>a;
+    // int sum = 0;
+    // int i = 1;
+    // while(i<=a){
+    //     sum = i+sum;
+    //     i++;
+    // }
+    // cout<<"Sum of given number is : " << sum <<endl;
+
+    int n;
+    cin >> n;
+    int sum = 0;
+    int b = 2;
+    while(b<=n){
+        sum = sum + b;
+        b++;
+    }
+    cout << sum;    
+}
