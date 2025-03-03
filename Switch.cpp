781cf2103742ae042aba8b969e6d94c91d6f5282
Kartik Pandey <karthikpandey580@gmail.com>
2025-03-03 10:15:26 +0530

    Create Switch.cpp

---
+++ Switch.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void whichWeekDay(int day) {
+        switch(day){
+            case 1:
+                std::cout << "Monday";
+                break;
+            case 2:
+                std::cout << "Tuesday";
+                break;
+            case 3:
+                std::cout << "Wednesday";
+                break;
+            case 4:
+                std::cout << "Thursday";
+                break;
+            case 5:
+                std::cout << "Friday";
+                break;
+            case 6:
+                std::cout << "Saturday";
+                break;
+            case 7:
+                std::cout << "Sunday";
+                break;
+            default:
+                std::cout << "Invalid";
+                break;
+        }
+    }
+};
